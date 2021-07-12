72c472cdd77d8500d4492d2c0cadc134001db587
Gisselle <glaffita@uci.cu>
2021-07-12 11:30:46 +0300

    Scrolling on the page

---
+++ src/main/java/com/herokuapp/theinternet/pages/WelcomePage.java
@@ -0,0 +1,92 @@
+package com.herokuapp.theinternet.pages;
+
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+public class WelcomePage extends BasePageObject {
+
+	private String pageURL = "http://the-internet.herokuapp.com/";
+
+	private By formAuthenticationLinkLocator = By.linkText("Form Authentication");
+	private By checkBoxesLinkLocator = By.linkText("Checkboxes");
+	private By dropDownLinkLocator = By.linkText("Dropdown");
+	private By javaScriptLinkLocator = By.linkText("JavaScript Alerts");
+	private By javaMultipleWindowLocator = By.linkText("Multiple Windows");
+	private By editorLocator = By.linkText("WYSIWYG Editor");
+	private By pressesKeyLocator = By.linkText("Key Presses");
+	private By uploadFileLocator = By.linkText("File Upload");
+
+	public WelcomePage(WebDriver driver, Logger log) {
+		super(driver, log);
+	}
+
+	/** Open Welcome page with its URL */
+	public void openPage() {
+		log.info("Opening page: " + pageURL);
+		openUrl(pageURL);
+		log.info("Page opened");
+	}
+
+	/** Open LoginPage by clicking on form Authentication link */
+	public LoginPage clickFormAuthenticationLink() {
+		log.info("Clicking FormAuthentication Link on Welcome Page");
+		click(formAuthenticationLinkLocator);
+		return new LoginPage(driver, log);
+	}
+
+	/** Open CheckBoxesPage by clicking on Check Boxes link */
+	public CheckBoxesPage clickCheckBoxesLink() {
+		log.info("Clicking CheckBoxes Link on Welcome Page");
+		click(checkBoxesLinkLocator);
+		return new CheckBoxesPage(driver, log);
+	}
+
+	/** Open DropDownPage by clicking on DropDown link */
+	public DropDownPage clickDropDownLink() {
+		log.info("Clicking DropDown Link on Welcome Page");
+		click(dropDownLinkLocator);
+		return new DropDownPage(driver, log);
+	}
+
+	/** Open MultipleWindowPage by clicking on MultipleWindow link */
+	public MultipleWindowPage clickMultipleWindowLink() {
+		log.info("Clicking MultipleWindow Link on Welcome Page");
+		click(javaMultipleWindowLocator);
+		return new MultipleWindowPage(driver, log);
+	}
+
+	/** Open JavaScriptAlertsPage by clicking on JavaScriptAlerts link */
+	public JavaScriptsAlertsPage clickJavaScriptAlertsLink() {
+		log.info("Clicking JavaScriptAlerts Link on Welcome Page");
+		click(javaScriptLinkLocator);
+		return new JavaScriptsAlertsPage(driver, log);
+	}
+
+	/** Open Editor Page by clicking on WYSIWYGEditor link */
+	public EditorPage clickWYSIWYGEditor() {
+		find(editorLocator).click();
+		return new EditorPage(driver, log);
+	}
+
+	/** Open Key Presses Page by clicking on KeyPresses link */
+	public KeyPressesPage clickPressesKey() {
+		find(pressesKeyLocator).click();
+		return new KeyPressesPage(driver, log);
+	}
+
+	/** Open Upload files Page by clicking on UploadFiles link */
+	public UploadingFilesPage clickFileUploadLink() {
+		find(uploadFileLocator).click();
+		return new UploadingFilesPage(driver, log);
+	}
+
+	/** Scroll to WYSIWYGEditor */
+	public void scrollToWYSIWYGEditorLink() {
+		log.info("Scrolling to the botton to the page ");
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+	}
+
+}
--- src/test/java/com/herokuapp/theinternet/editortests/EditorTests.java
+++ src/test/java/com/herokuapp/theinternet/editortests/EditorTests.java
@@ -5,17 +5,22 @@ import org.testng.annotations.Test;
 
 import com.herokuapp.theinternet.base.TestUtilities;
 import com.herokuapp.theinternet.pages.EditorPage;
-import com.herokuapp.theinternet.pages.WelcomePageObject;
+import com.herokuapp.theinternet.pages.WelcomePage;
 
 public class EditorTests extends TestUtilities {
 
 	@Test
 	public void defaultEditorValueTest() {
 		log.info("Starting defaultEditorValueTest");
-		
+
 		// open main page
-		WelcomePageObject wp = new WelcomePageObject(driver, log);
+		WelcomePage wp = new WelcomePage(driver, log);
 		wp.openPage();
+
+		// Scroll with Java Script Executor
+		sleep(5000);
+		wp.scrollToWYSIWYGEditorLink();
+		sleep(5000);
 
 		// Click on "WYSIWYG Editor" link witch open new page WYSIWYG Editor
 		EditorPage ep = wp.clickWYSIWYGEditor();
@@ -26,5 +31,14 @@ public class EditorTests extends TestUtilities {
 		// Verification that new page contains expected text in source
 		Assert.assertTrue(editorText.equals("Your content goes here."),
 				"Editor default textis not expected. It is: " + editorText);
+	}
+
+	public void sleep(int n) {
+		try {
+			Thread.sleep(n);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
