5ad5e37af4ca94f626dfa778f45eb782a7f0ccf5
Gisselle <glaffita@uci.cu>
2021-07-16 13:33:16 +0300

    Running test with csv DataProvider

--- src/test/java/com/herokuapp/theinternet/loginpagetests/NegativeLogInTests.java
+++ src/test/java/com/herokuapp/theinternet/loginpagetests/NegativeLogInTests.java
@@ -1,19 +1,27 @@
 package com.herokuapp.theinternet.loginpagetests;
 
+import java.util.Map;
+
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.herokuapp.theinternet.base.CsvDataProviders;
 import com.herokuapp.theinternet.base.TestUtilities;
 import com.herokuapp.theinternet.pages.LoginPage;
 import com.herokuapp.theinternet.pages.WelcomePage;
 
 public class NegativeLogInTests extends TestUtilities {
 
-	@Parameters({ "username", "password", "expectedMessage" })
-	@Test(priority = 1)
-	public void negativeTest(String username, String password, String expectedErrorMessage) {
-		log.info("Starting negativeTest");
+	@Test(priority = 1, dataProvider = "csvReader", dataProviderClass = CsvDataProviders.class)
+	public void negativeLogInTest(Map<String, String> testData) {
+		// Data
+		String no = testData.get("no");
+		String username = testData.get("username");
+		String password = testData.get("password");
+		String expectedErrorMessage = testData.get("expectedMessage");
+		String description = testData.get("description");
+
+		log.info("Starting negativeLogInTest #" + no + " for " + description);
 
 		// open main page
 		WelcomePage welcomePage = new WelcomePage(driver, log);
@@ -28,7 +36,7 @@ public class NegativeLogInTests extends TestUtilities {
 		// Wait for error message
 		loginPage.waitForErrorMessage();
 		String message = loginPage.getErrorMessage();
-		
+
 		// Verification
 		Assert.assertTrue(message.contains(expectedErrorMessage), "Message doesn't contain expected text");
 	}
